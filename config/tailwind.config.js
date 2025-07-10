e21a186d5b60de19a7e59843f0f93abb22721ccd
Vinay Kapoor <kapoor.vinay@gmail.com>
2025-07-10 13:44:45 -0400

    Latest changes to add debug logging

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,7 +7,16 @@ module.exports = {
     path.join(__dirname, '../api/*.js')
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        expana : {
+        green: '#cee7d5',
+        darkgreen: '#06321e',
+        hovergreen: '#0f7d4b',
+        textmain: '#1a212f'
+        }
+      }
+    },
   },
   plugins: [],
 };
